b8b1392ba00bfd46bd04bcd13ad383f38c7a33c6
00sleng00 <roman.morshikoff@yandex.ru>
2023-05-11 13:20:06 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,51 @@
+import { Telegraf, session } from 'telegraf'
+import { message } from 'telegraf/filters'
+import { code } from 'telegraf/format'
+import config from 'config'
+import { ogg } from './ogg.js'
+import { openai } from './openai.js'
+import { removeFile } from './utils.js'
+import { initCommand, processTextToChat, INITIAL_SESSION } from './logic.js'
+
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
+
+bot.use(session())
+
+bot.command('new', initCommand)
+
+bot.command('start', initCommand)
+
+bot.on(message('voice'), async (ctx) => {
+   ctx.session ??= INITIAL_SESSION
+   try {
+      await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
+      const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+      const userId = String(ctx.message.from.id)
+      const oggPath = await ogg.create(link.href, userId)
+      const mp3Path = await ogg.toMp3(oggPath, userId)
+
+      removeFile(oggPath)
+
+      const text = await openai.transcription(mp3Path)
+      await ctx.reply(code(`Ваш запрос: ${text}`))
+
+      await processTextToChat(ctx, text)
+   } catch (e) {
+      console.log(`Error while voice message`, e.message)
+   }
+})
+
+bot.on(message('text'), async (ctx) => {
+   ctx.session ??= INITIAL_SESSION
+   try {
+      await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
+      await processTextToChat(ctx, ctx.message.text)
+   } catch (e) {
+      console.log(`Error while voice message`, e.message)
+   }
+})
+
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
---
+++ src/utils.js
@@ -0,0 +1,10 @@
+import { unlink } from 'fs/promises'
+
+export async function removeFile(path) {
+   try {
+      await unlink(path)
+   } catch (e) {
+      console.log('Error while removing file', e.message)
+   }
+}
+
